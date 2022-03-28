6d7746dea38eae840ca9e7ebc309170d059e9b64
Pauline2706 <102090776+Pauline2706@users.noreply.github.com>
2022-03-28 20:12:10 +0300

    Add files via upload

---
+++ program to accept two integers.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1, num2, product;
+    printf("Enter two integer values/n");
+    scanf("%d",num1 & num2);
+    product=num1 * num2;
+    printf("product is %d", product);
+    return 0;
+}
---
+++ sum of five numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int sum=0,j,b;
+for(i=0;i<5;i++)
+{
+scanf("%d",&b);
+sum=sum+b;
+}
+printf("\nThe sum is %d",sum);
+return 0;
+}
